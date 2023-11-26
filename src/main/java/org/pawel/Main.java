fab9a09d0ac566d0ff0bc5a43c2bf3332741e0f3
Pawel Szalawinski <pawel.szalawinski@tink.com>
2023-11-26 18:35:50 +0100

    refactored Main and Checker

--- src/main/java/org/pawel/Main.java
+++ src/main/java/org/pawel/Main.java
@@ -3,16 +3,14 @@ package org.pawel;
 import org.pawel.adapter.CheckerController;
 import org.pawel.service.NameCheckerService;
 import org.pawel.service.SsnCheckerService;
-import org.pawel.validators.LuhnChecksumCalculator;
-import org.pawel.validators.SsnValidator;
 
 public class Main {
 
 	public static void main(String[] args) {
 		CheckerController checkerController = new CheckerController(
-				new SsnCheckerService(
-						new SsnValidator(
-								new LuhnChecksumCalculator())), new NameCheckerService());
+				new SsnCheckerService(),
+				new NameCheckerService());
+
 		checkerController.chooseChecker();
 	}
 }
--- src/main/java/org/pawel/adapter/CheckerController.java
+++ src/main/java/org/pawel/adapter/CheckerController.java
@@ -1,5 +1,7 @@
 package org.pawel.adapter;
 
+import static org.pawel.utils.Constants.*;
+
 import java.util.Arrays;
 import java.util.Scanner;
 import org.pawel.service.NameCheckerService;
@@ -18,44 +20,44 @@ public class CheckerController {
 	}
 
 	public void chooseChecker() {
-		System.out.println("Do you want to check SSN [1] or name [2]? For exit, type exit.");
+		System.out.println(MAIN_MESSAGE);
 		String input = scanner.nextLine();
-		while (!input.equals("exit")) {
+		while (!input.equals(EXIT)) {
 			switch (input) {
 				case "1" -> checkSsnNumber();
 				case "2" -> checkName();
 			}
-			System.out.println("Do you want to check SSN [1] or name [2]? For exit, type exit.");
+			System.out.println(MAIN_MESSAGE);
 			input = scanner.nextLine();
 		}
 
 	}
 
 	public void checkSsnNumber() {
-		System.out.println("Provide SSN or type exit");
+		System.out.println(SSN_OR_EXIT);
 		String input = scanner.nextLine();
 
-		while (!input.equals("exit")) {
+		while (!input.equals(EXIT)) {
 			if (
 					checkerService.isValidSsnNumber(input)) {
 				System.out.println("Given SSN is valid ".concat(
-						Arrays.toString(Character.toChars(0x2705))));
+						Arrays.toString(CORRECT)));
 			}
-			System.out.println("Provide SSN or type exit");
+			System.out.println(SSN_OR_EXIT);
 			input = scanner.nextLine();
 		}
 	}
 
 	public void checkName() {
-		System.out.println("Provide name or type exit");
+		System.out.println(NAME_OR_EXIT);
 		String input = scanner.nextLine();
 
-		while (!input.equals("exit")) {
+		while (!input.equals(EXIT)) {
 			if (nameCheckerService.isValidName(input)) {
 				System.out.println("Given Name is valid ".concat(
-						Arrays.toString(Character.toChars(0x2705))));
+						Arrays.toString(CORRECT)));
 			}
-			System.out.println("Provide name or type exit");
+			System.out.println(NAME_OR_EXIT);
 			input = scanner.nextLine();
 		}
 	}
