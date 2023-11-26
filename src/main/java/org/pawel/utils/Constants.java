42b8627c4c0dfd0a38d747d2c48a155827507a03
Pawel Szalawinski <pawel.szalawinski@tink.com>
2023-11-26 18:34:45 +0100

    added constants

---
+++ src/main/java/org/pawel/utils/Constants.java
@@ -0,0 +1,11 @@
+package org.pawel.utils;
+
+public class Constants {
+
+	public static char[] WRONG = Character.toChars(0x274C);
+	public static char[] CORRECT = Character.toChars(0x2705);
+	public static String MAIN_MESSAGE = "---Do you want to check SSN [1] or name [2]? For exit, type exit.---";
+	public static String EXIT = "exit";
+	public static String NAME_OR_EXIT = "Provide name or type exit";
+	public static String SSN_OR_EXIT = "Provide SSN or type exit";
+}
